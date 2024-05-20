213cb62bdcadaeffbf564163a6605bcfff7f16e9
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-05-20 14:55:51 +0100

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+	name: "Frikanza",
+	url: "https://frikanza.vercel.app",
+	ogImage: "https://frikanza.vercel.app/og.jpg",
+	description:
+		"Discover unparalleled luxury and elegance with Mansa Musa Couture. Our exclusive collection of high-end fashion blends timeless sophistication with modern design. Elevate your style with our exquisite pieces, crafted to perfection for the discerning individual.",
+	links: {
+		twitter: "https://twitter.com/hussain_joe",
+		github: "https://github.com/uy-scuti100",
+	},
+};
