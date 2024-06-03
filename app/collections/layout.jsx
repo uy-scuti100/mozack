812ac7ec68019cabc63a2136d5da276bbffcb169
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-06-03 10:39:26 +0100

    retrying the collections page matadata error fix

--- app/collections/layout.jsx
+++ app/collections/layout.jsx
@@ -1,23 +1,23 @@
-// export const metadata = {
-// 	title: "Collections",
-// 	description:
-// 		"Explore Mozack's premium furniture collections, blending luxury with functionality. Elevate your home decor effortlessly. Discover now!",
-// 	openGraph: {
-// 		images: [
-// 			"https://i.pinimg.com/564x/fb/e9/4f/fbe94f6c483f0108374526919737b3a8.jpg",
-// 		],
-// 	},
-// 	twitter: {
-// 		card: "summary_large_image",
-// 		site: "@hussain_joe",
-// 		creator: "@hussain_joe",
-// 		title: "Collections",
-// 		description:
-// 			"Explore Mozack's premium furniture collections, blending luxury with functionality. Elevate your home decor effortlessly. Discover now!",
-// 		image:
-// 			"https://i.pinimg.com/564x/fb/e9/4f/fbe94f6c483f0108374526919737b3a8.jpg",
-// 	},
-// };
+export const metadata = {
+	title: "Collections",
+	description:
+		"Explore Mozack's premium furniture collections, blending luxury with functionality. Elevate your home decor effortlessly. Discover now!",
+	openGraph: {
+		images: [
+			"https://i.pinimg.com/564x/fb/e9/4f/fbe94f6c483f0108374526919737b3a8.jpg",
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: "@hussain_joe",
+		creator: "@hussain_joe",
+		title: "Collections",
+		description:
+			"Explore Mozack's premium furniture collections, blending luxury with functionality. Elevate your home decor effortlessly. Discover now!",
+		image:
+			"https://i.pinimg.com/564x/fb/e9/4f/fbe94f6c483f0108374526919737b3a8.jpg",
+	},
+};
 export default function CollectionLayout({ children }) {
 	return <section>{children}</section>;
 }
--- todo.md
+++
@@ -1,6 +0,0 @@
-build the single product page
-make the search bar work and be dynamic
-make the ribbons
-New Arrivals
-Bestsellers
-Essentials dynamic
